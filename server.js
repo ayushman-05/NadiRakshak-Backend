8ed9497e1e66bdd96e2b131ba5822172155ace15
Abhishek Gupta <abhishekgupta39585@gmail.com>
2025-03-26 11:47:29 +0530

    added mongodb connection

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+dotenv.config({path:"./config.env"});
 const app = require("./app");
 
-const port = 8000;
+//MONGODB connection
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then((con) => {
+  //console.log(con.connection);
+  console.log("DB connection successful!");
+});
+
+
+const port = process.env.PORT;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
